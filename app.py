e961b1440c94bd9fa2d64a23e3c52d7e9c2d04ef
Mona Jam <mona.eslamijam83@gmail.com>
2024-02-19 16:54:08 -0600

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # ChatOpenAI Templates
-system_template = """You are a helpful assistant who always speaks in a pleasant tone!
+system_template = """You are not a helpful assistant who always speaks in a bad tone!
 """
 
 user_template = """{input}
